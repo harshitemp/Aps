4e81064c28e300e07ad8e7dad3c0f3059e9ddbb4
MOHITH CHOWDARY CHAVA <89778762+MOHITH195@users.noreply.github.com>
2023-03-13 10:27:53 +0530

    Initial commit

---
+++ connect.php
@@ -0,0 +1,33 @@
+ <?php
+// echo $_SERVER['REQUEST_METHOD'] ;
+// if($_SERVER(['REQUEST_METHOD'] == 'POST')){
+// $name=$_POST(['name']);
+// $number=$_POST(['number']);
+// $password=$_POST(['password']);
+
+// $connection= mysqli_connect('localhost','root','','hms');
+
+// if($connection->connect_error)
+// {
+//     die('Connection Failed :'.$conn->connect_error);
+// }
+// else
+// {
+//     echo "connection success";
+//     $hello= $connection->prepare("INSERT INTO login(login_id,login_username,login_password) VALUES($number,$name,$password)");
+//     $hello->bind_param("iss",$number,$name,$password);
+//     $hello->execute;
+//     $hello->close();
+//     $connection->close();
+
+// }
+
+// }
+
+
+
+?> 
+
+
+
+
